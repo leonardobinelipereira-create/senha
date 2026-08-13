f6330243ea30c21a3a02cc907b662e7dccead133
leonardobinelipereira-create <leonardo.bineli.pereira@escola.pr.gov.br>
2026-08-13 10:57:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -38,6 +38,21 @@ function geraSenha(){
     }
     campoSenha.value = senha;
 }
+// código omitido
 
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha(){
+ for (let i = 0; i < tamanhoSenha;i++) {
+     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+     numeroAleatorio = Math.floor(numeroAleatorio);
+     console.log(letrasMaiusculas[numeroAleatorio]);
+ }
+}
+
+campoSenha.value = letrasMaiusculas;
 
 
