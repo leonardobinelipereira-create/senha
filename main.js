a93ddd39cc6d487b49b1709bdecc1948850c717a
leonardobinelipereira-create <leonardo.bineli.pereira@escola.pr.gov.br>
2026-08-20 10:51:20 -0300

    senha

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    valorEntropia.textContent = "um compultador pode levar até" Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha."; 
 }
 
 
